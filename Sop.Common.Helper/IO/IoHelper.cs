d434f0dd299c060410526b2041de2fe8265588f8
guojiaqiu <sopcce@qq.com>
2020-04-28 09:06:49 +0800

    add io code

---
+++ Sop.Common.Helper/IO/IoHelper.cs
@@ -0,0 +1,501 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Sop.Common.Helper.IO
+{
+    public static class IoHelper
+    {
+        public static readonly Encoding GB2312 = Encoding.GetEncoding(936);
+
+        public static readonly string AllowUploadOrDeleteFileTypes = string.Empty;
+
+        public const string AllowReadeOrWriteFileTypes = ".html|.htm|.txt|.css|.js|.log|.xml";
+
+        public const string AllowAdFileTypes = ".jpg|.jpeg|.gif|.png|.swf";
+
+        public const string AllowImageAndMediasFileTypes = ".jpg|.jpeg|.gif|.png|.swf|.mid|.wav|.mp3";
+
+        public const string AllowImageFileTypes = ".jpg|.jpeg|.gif|.png";
+
+        public const string SerializedFileExtension = ".sop";
+
+        public static bool IsAllowedFileType(string FileName, string FileTypes)
+        {
+            if (string.IsNullOrEmpty(FileName))
+            {
+                return false;
+            }
+
+            string text = Path.GetExtension(FileName).ToLower();
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            FileTypes = $"|{FileTypes}|".ToLower();
+            if (!FileTypes.Contains($"|{text}|"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string ReadTextFile(string FilePath, int EncodingCodepage)
+        {
+            if (!IsAllowedFileType(FilePath, ".html|.htm|.txt|.css|.js|.log|.xml"))
+            {
+                return string.Empty;
+            }
+
+            if (!File.Exists(FilePath))
+            {
+                return string.Empty;
+            }
+
+            string empty = string.Empty;
+            Encoding encoding = (EncodingCodepage != 0) ? Encoding.GetEncoding(EncodingCodepage) : GB2312;
+            try
+            {
+                using (FileStream stream =
+                    new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (StreamReader streamReader = new StreamReader(stream, encoding))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static bool WriteTextFile(string FilePath, string Content, int EncodingCodepage)
+        {
+            if (!IsAllowedFileType(FilePath, ".html|.htm|.txt|.css|.js|.log|.xml"))
+            {
+                return false;
+            }
+
+            bool flag = false;
+            try
+            {
+                using (FileStream stream = new FileStream(FilePath, FileMode.Create, FileAccess.Write))
+                {
+                    Encoding encoding = Encoding.GetEncoding(EncodingCodepage);
+                    using (StreamWriter streamWriter = new StreamWriter(stream, encoding))
+                    {
+                        streamWriter.Write(Content);
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static bool WriteTextFileByAppend(string FilePath, string Content, int EncodingCodepage)
+        {
+            if (!IsAllowedFileType(FilePath, ".html|.htm|.txt|.css|.js|.log|.xml"))
+            {
+                return false;
+            }
+
+            bool flag = false;
+            try
+            {
+                using (FileStream stream = new FileStream(FilePath, FileMode.Append, FileAccess.Write))
+                {
+                    Encoding encoding = Encoding.GetEncoding(EncodingCodepage);
+                    using (StreamWriter streamWriter = new StreamWriter(stream, encoding))
+                    {
+                        streamWriter.Write(Content);
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static bool DeleteFile(string FilePath)
+        {
+            if (!IsAllowedFileType(FilePath, AllowUploadOrDeleteFileTypes))
+            {
+                return false;
+            }
+
+            bool result = true;
+            try
+            {
+                File.Delete(FilePath);
+                return result;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool MoveOrRenameFile(string FilePath, string NewFilePath)
+        {
+            if (!IsAllowedFileType(FilePath, AllowUploadOrDeleteFileTypes))
+            {
+                return false;
+            }
+
+            if (File.Exists(NewFilePath))
+            {
+                return false;
+            }
+
+            bool result = false;
+            try
+            {
+                if (File.Exists(FilePath))
+                {
+                    File.Move(FilePath, NewFilePath);
+                    return true;
+                }
+
+                return result;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static void CopyFile(string SrcFilePath, string DestFilePath)
+        {
+            if (File.Exists(SrcFilePath))
+            {
+                File.Copy(SrcFilePath, DestFilePath, overwrite: true);
+            }
+        }
+
+        public static string GenerateNewRandomFilename(string OriginalFilename)
+        {
+            Random random = new Random();
+            return string.Format("{0}{1}", DateTime.Now.ToString("yyyyMMddHHmmssffff"), random.Next(0, 10)) +
+                   Path.GetExtension(OriginalFilename);
+        }
+
+        public static string GetFileExtension(string FilePath)
+        {
+            string text = Path.GetExtension(FilePath);
+            if (text == null)
+            {
+                text = string.Empty;
+            }
+
+            return text.ToLower();
+        }
+
+        public static string[] GetFileInfo(string FilePhysicalPath)
+        {
+            string[] obj = new string[3]
+            {
+                "0",
+                null,
+                null
+            };
+            DateTime minValue = DateTime.MinValue;
+            obj[1] = minValue.ToString();
+            obj[2] = "";
+            string[] array = obj;
+            if (string.IsNullOrEmpty(FilePhysicalPath))
+            {
+                return array;
+            }
+
+            FileInfo fileInfo = new FileInfo(FilePhysicalPath);
+            if (!fileInfo.Exists)
+            {
+                return array;
+            }
+
+            array[0] = fileInfo.Length.ToString();
+            DateTime lastWriteTime = fileInfo.LastWriteTime;
+            array[1] = lastWriteTime.ToString("yyyy-MM-dd HH:mm:ss");
+            array[2] = $"\"{lastWriteTime.Ticks}\"";
+            return array;
+        }
+
+        public static bool FileExists(string FilePath)
+        {
+            return File.Exists(FilePath);
+        }
+
+        public static byte[] ReadByteArrayFromStream(Stream InputStream)
+        {
+            try
+            {
+                byte[] array = new byte[InputStream.Length];
+                int i = 0;
+                for (int num = array.Length; i < num; i += InputStream.Read(array, i, num - i))
+                {
+                }
+
+                return array;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static string GetRandomCharsCanUseForFilename(int Length)
+        {
+            if (Length <= 0)
+            {
+                return string.Empty;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            while (stringBuilder.Length < Length)
+            {
+                stringBuilder.Append(Path.GetRandomFileName().Replace(".", ""));
+            }
+
+            return stringBuilder.ToString().Substring(0, Length);
+        }
+
+        public static T ReadSerializedData<T>(string FilePath) where T : class
+        {
+            if (!Path.HasExtension(FilePath))
+            {
+                FilePath = string.Format("{0}{1}", FilePath, ".yps");
+            }
+
+            if (!File.Exists(FilePath))
+            {
+                return null;
+            }
+
+            T result = null;
+            try
+            {
+                using (FileStream serializationStream =
+                    new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    result = (T) new BinaryFormatter().Deserialize(serializationStream);
+                    return result;
+                }
+            }
+            catch
+            {
+                return result;
+            }
+        }
+
+        public static bool WriteSerializedData<T>(string FilePath, T SerializableObject) where T : class
+        {
+            if (!Path.HasExtension(FilePath))
+            {
+                FilePath = string.Format("{0}{1}", FilePath, ".yps");
+            }
+
+            bool result = true;
+            try
+            {
+                using (FileStream serializationStream = new FileStream(FilePath, FileMode.Create))
+                {
+                    new BinaryFormatter().Serialize(serializationStream, SerializableObject);
+                    return result;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteSerializedFile(string FilePath)
+        {
+            if (!Path.HasExtension(FilePath))
+            {
+                FilePath = string.Format("{0}{1}", FilePath, ".yps");
+            }
+            else if (!Path.GetExtension(FilePath).ToLower().Equals(".yps"))
+            {
+                return false;
+            }
+
+            bool result = true;
+            try
+            {
+                File.Delete(FilePath);
+                return result;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool FolderExists(string DirPath)
+        {
+            return Directory.Exists(DirPath);
+        }
+
+        public static string[] GetAllFileNamesOfDir(string DirPath)
+        {
+            try
+            {
+                return Directory.GetFiles(DirPath);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static FileInfo[] GetAllFilesInfoOfDir(string DirPath)
+        {
+            try
+            {
+                FileInfo[] result = null;
+                DirectoryInfo directoryInfo = new DirectoryInfo(DirPath);
+                if (directoryInfo.Exists)
+                {
+                    result = directoryInfo.GetFiles();
+                }
+
+                return result;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static string[] GetAllFoldersOfDir(string DirPath)
+        {
+            try
+            {
+                return Directory.GetDirectories(DirPath);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static DirectoryInfo[] GetAllFoldersInfoOfDir(string DirPath)
+        {
+            try
+            {
+                DirectoryInfo[] result = null;
+                DirectoryInfo directoryInfo = new DirectoryInfo(DirPath);
+                if (directoryInfo.Exists)
+                {
+                    result = directoryInfo.GetDirectories();
+                }
+
+                return result;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static bool CreateDir(string DirPath)
+        {
+            if (Directory.Exists(DirPath))
+            {
+                return false;
+            }
+
+            try
+            {
+                Directory.CreateDirectory(DirPath);
+            }
+            catch
+            {
+                throw;
+            }
+
+            return true;
+        }
+
+        public static bool DeleteDir(string DirPath)
+        {
+            try
+            {
+                if (Directory.Exists(DirPath))
+                {
+                    Directory.Delete(DirPath, recursive: true);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return true;
+        }
+
+        public static long GetDirSize(string DirPath, bool AndSubdirectorys)
+        {
+            long num = 0L;
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(DirPath);
+                FileInfo[] files = directoryInfo.GetFiles();
+                foreach (FileInfo fileInfo in files)
+                {
+                    num += fileInfo.Length / 1024 + 1;
+                }
+
+                if (!AndSubdirectorys)
+                {
+                    return num;
+                }
+
+                DirectoryInfo[] directories = directoryInfo.GetDirectories();
+                foreach (DirectoryInfo directoryInfo2 in directories)
+                {
+                    num += GetDirSize(directoryInfo2.FullName, AndSubdirectorys: true);
+                }
+
+                return num;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static string DirectoryName(string DirPath)
+        {
+            return Path.GetFileName(DirPath.Trim('\\', ' '));
+        }
+
+        public static string GetParentDirectoryPath(string DirPath, string LockInBasePath)
+        {
+            if (string.IsNullOrEmpty(LockInBasePath))
+            {
+                return DirPath;
+            }
+
+            string text = DirPath.TrimEnd('\\', ' ');
+            text = text.Substring(0, text.LastIndexOf("\\") + 1);
+            if (!text.StartsWith(LockInBasePath, StringComparison.CurrentCultureIgnoreCase))
+            {
+                text = DirPath;
+            }
+
+            return text;
+        }
+    }
+}
---
+++ Sop.Common.Helper/Utility/HttpUtility.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace Sop.Common.Helper.Utility
+{
+    public class HttpUtility
+    {
+        /// <summary>
+        ///  发送POST 请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="jsonData"></param> 
+        /// <returns></returns>
+        public static string Post(string url, string jsonData)
+        {
+            url = (url.IndexOf("http://", StringComparison.Ordinal) > -1 || url.IndexOf("https://", StringComparison.Ordinal) > -1) ? url : "http://" + url;
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback += (s, cert, chain, sslPolicyErrors) => true;
+
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(url));
+                httpWebRequest.ServicePoint.Expect100Continue = true;
+                httpWebRequest.ServicePoint.UseNagleAlgorithm = false;
+                httpWebRequest.ServicePoint.ConnectionLimit = int.MaxValue;
+                httpWebRequest.AllowWriteStreamBuffering = false;
+                httpWebRequest.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+                httpWebRequest.AllowAutoRedirect = false;
+                httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36";
+                httpWebRequest.Accept =
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
+                httpWebRequest.ContentType = "application/json;charset=utf-8";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Timeout = 1000 * 60 * 5;
+                byte[] data = Encoding.UTF8.GetBytes(jsonData);
+                httpWebRequest.ContentLength = data.Length;
+                using (Stream reqStream = httpWebRequest.GetRequestStream())
+                {
+                    reqStream.Write(data, 0, data.Length);
+                    reqStream.Close();
+                }
+
+                List<byte> lst = new List<byte>();
+                int nRead = 0;
+                string strHtml = string.Empty;
+                using (HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+
+                    // 判断是否重定向  Ambiguous 300  Found 302  Moved 301
+                    if (httpWebResponse.StatusCode == HttpStatusCode.Ambiguous ||
+                        httpWebResponse.StatusCode == HttpStatusCode.Found ||
+                        httpWebResponse.StatusCode == HttpStatusCode.Moved)
+                    {
+                        string newUrl = httpWebResponse.Headers["Location"];//获取重定向的网址
+                        if (!string.IsNullOrEmpty(newUrl))
+                        {
+                            httpWebResponse.Close();
+                            return Post(newUrl, jsonData);
+                        }
+                    }
+                    if (httpWebResponse.StatusCode == HttpStatusCode.OK)
+                    {
+
+                        if (httpWebResponse.ContentEncoding.ToLower() == "gzip")//如果使用了GZip则先解压
+                        {
+                            using (System.IO.Stream streamReceive = httpWebResponse.GetResponseStream())
+                            {
+                                using (var stream = new GZipStream(streamReceive, CompressionMode.Decompress))
+                                {
+                                    while ((nRead = stream.ReadByte()) != -1) lst.Add((byte)nRead);
+                                    byte[] byHtml = lst.ToArray();
+                                    strHtml = Encoding.UTF8.GetString(byHtml, 0, byHtml.Length);
+                                }
+                            }
+                        }
+                        else if (httpWebResponse.ContentEncoding.ToLower().Contains("deflate"))//解压
+                        {
+                            using (Stream streamReceive = httpWebResponse.GetResponseStream())
+                            {
+                                using (var stream = new DeflateStream(streamReceive, CompressionMode.Decompress))
+                                {
+
+                                    while ((nRead = stream.ReadByte()) != -1) lst.Add((byte)nRead);
+                                    byte[] byHtml = lst.ToArray();
+                                    strHtml = Encoding.UTF8.GetString(byHtml, 0, byHtml.Length);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            using (Stream stream = httpWebResponse.GetResponseStream())
+                            {
+                                while (stream != null && (nRead = stream.ReadByte()) != -1) lst.Add((byte)nRead);
+                                byte[] byHtml = lst.ToArray();
+                                strHtml = Encoding.UTF8.GetString(byHtml, 0, byHtml.Length);
+                            }
+                        }
+                    }
+                    httpWebResponse.Close();
+                }
+                return strHtml;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
